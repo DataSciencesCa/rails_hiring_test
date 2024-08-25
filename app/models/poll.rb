2b792101d7641feafb9e7c7e89a2e71842af4072
Dmytro Liaska <dmytro.liaska@homestars.com>
2024-08-25 11:38:21 -0400

    add edit and update polling info forms
    update styles and add multiselect
    
    code cleanup and tests
    
    Remove all files
    
    Empty commit

--- app/models/poll.rb
+++
@@ -1,6 +0,0 @@
-class Poll < ApplicationRecord
-  belongs_to :riding
-  belongs_to :polling_location, optional: true
-
-  validates :number, presence: true
-end
--- app/models/riding.rb
+++
@@ -1,8 +0,0 @@
-class Riding < ApplicationRecord
-  has_many :polls, dependent: :destroy
-  has_many :polling_locations, dependent: :destroy
-
-  validates :name, presence: true
-  validates :riding_code, presence: true
-  validates :province, presence: true
-end
